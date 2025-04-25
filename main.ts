e759189eea1c742bc552011c0d2c3dd116f08c40
Omkar Date <omkardate29@gmail.com>
2025-04-25 23:27:12 +0530

    chat

--- main.ts
+++ main.ts
@@ -1,16 +1,56 @@
 import { google } from "@ai-sdk/google";
-import { streamText } from "ai";
+import { CoreMessage, generateText } from "ai";
+import readline from "readline";
 
-async function main() {
-  const { textStream } = streamText({
-    model: google("gemini-1.5-pro-latest"),
-    prompt:
-      "Invent a new holiday in Indian calender and describe its traditions.",
-  });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-  for await (const textPart of textStream) {
-    process.stdout.write(textPart);
+const messagesToSend: CoreMessage[] = [
+  {
+    role: "system",
+    content:
+      "You are a helpful assistant that can generate text based on the given prompt and make sure to stick to the shorter answers.",
+  },
+];
+
+console.log("Chat started!");
+
+rl.setPrompt("You: ");
+rl.prompt();
+
+rl.on("line", async (input) => {
+  if (input.trim().toLowerCase() === "exit") {
+    console.log("Exiting chat...");
+    rl.close();
+    return;
   }
-}
 
-main();
+  if (input.trim().length === 0) {
+    console.log("Please enter a valid input.");
+    rl.prompt();
+    return;
+  }
+
+  messagesToSend.push({ role: "user", content: input });
+
+  try {
+    const { text } = await generateText({
+      model: google("gemini-2.0-flash-exp"),
+      messages: messagesToSend,
+    });
+
+    console.log("Assistant: ", text);
+    messagesToSend.push({ role: "assistant", content: text });
+
+    // console.log("Messages sent so far: ", messagesToSend);
+  } catch (error) {
+    console.log(error);
+
+    rl.prompt();
+    return;
+  }
+
+  rl.prompt();
+});
